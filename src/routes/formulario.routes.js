8c7b59c703ee8b1adc5233473b27c6e01ce23e2e
arnold <luizarnolch@gmai.com>
2021-07-26 14:43:26 -0500

    [bug formulario]: resuelto

--- src/routes/formulario.routes.js
+++ src/routes/formulario.routes.js
@@ -8,15 +8,15 @@ router.get('/prenda/Carrito', Formulario.G_carrito);
 router.post('/prenda/Carrito', Formulario.P_carrito);
 
 //FORMULARIO DE DATOS
-router.get('/prenda/FormularioDato', Formuario.G_formulario_datos);
-router.post('/prenda/FormularioDato', Formuario.P_formulario_datos);
+router.get('/prenda/FormularioDato', Formulario.G_formulario_datos);
+router.post('/prenda/FormularioDato', Formulario.P_formulario_datos);
 
 //FORMULARIO DE ENVIO
-router.get('/prenda/FormularioEnvio', Formuario.G_formulario_envio);
-router.post('/prenda/FormularioEnvio', Formuario.P_formulario_envio);
+router.get('/prenda/FormularioEnvio', Formulario.G_formulario_envio);
+router.post('/prenda/FormularioEnvio', Formulario.P_formulario_envio);
 
 //FORMULARIO DE PAGO
-router.get('/prenda/FormularioPago', Formuario.G_formulario_pago);
-router.post('/prenda/FormularioPago', Formuario.P_formulario_pago);
+router.get('/prenda/FormularioPago', Formulario.G_formulario_pago);
+router.post('/prenda/FormularioPago', Formulario.P_formulario_pago);
 
 module.exports = router;
