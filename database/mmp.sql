e8a3699f596a2879d7296ff18d89a0b68c1dc462
arnold <luizarnolch@gmai.com>
2021-08-03 22:33:37 -0500

    DB solucion

--- database/mmp.sql
+++ database/mmp.sql
@@ -11,13 +11,13 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `heroku_9bb4630feaaa67c` DEFAULT CHARACTER SET utf8 ;
+USE `heroku_9bb4630feaaa67c` ;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`etiquetas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`etiquetas` (
+CREATE TABLE IF NOT EXISTS `heroku_9bb4630feaaa67c`.`etiquetas` (
     `idetiquetas` INT NOT NULL,
     `modelo` VARCHAR(45) NOT NULL,
     `color` VARCHAR(45) NOT NULL,
@@ -28,7 +28,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`imagenes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`imagenes` (
+CREATE TABLE IF NOT EXISTS `heroku_9bb4630feaaa67c`.`imagenes` (
     `idimagenes` INT NOT NULL,
     `ruta` LONGBLOB NOT NULL,
     PRIMARY KEY (`idimagenes`))
@@ -38,7 +38,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Producto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Producto` (
+CREATE TABLE IF NOT EXISTS `heroku_9bb4630feaaa67c`.`Producto` (
     `idProducto` INT NOT NULL AUTO_INCREMENT,
     `nombre` VARCHAR(255) NOT NULL,
     `descripcion` VARCHAR(1024) NOT NULL,
@@ -46,16 +46,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Producto` (
     `etiquetas_idcaracteristicas` INT NOT NULL,
     `imagenes_idimagenes` INT NOT NULL,
     PRIMARY KEY (`idProducto`),
-    INDEX `fk_Producto_etiquetas1_idx` (`etiquetas_idcaracteristicas` ASC) VISIBLE,
-    INDEX `fk_Producto_imagenes1_idx` (`imagenes_idimagenes` ASC) VISIBLE,
+    INDEX `fk_Producto_etiquetas1_idx` (`etiquetas_idcaracteristicas` ASC),
+    INDEX `fk_Producto_imagenes1_idx` (`imagenes_idimagenes` ASC),
     CONSTRAINT `fk_Producto_etiquetas1`
         FOREIGN KEY (`etiquetas_idcaracteristicas`)
-        REFERENCES `mydb`.`etiquetas` (`idetiquetas`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`etiquetas` (`idetiquetas`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION,
     CONSTRAINT `fk_Producto_imagenes1`
         FOREIGN KEY (`imagenes_idimagenes`)
-        REFERENCES `mydb`.`imagenes` (`idimagenes`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`imagenes` (`idimagenes`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -64,7 +64,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Medidas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Medidas` (
+CREATE TABLE IF NOT EXISTS `heroku_9bb4630feaaa67c`.`Medidas` (
     `idMedidas` INT NOT NULL,
     `altura` DECIMAL(10,2) NOT NULL,
     `busto` DECIMAL(10,2) NOT NULL,
@@ -77,7 +77,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Cliente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Cliente` (
+CREATE TABLE IF NOT EXISTS `heroku_9bb4630feaaa67c`.`Cliente` (
     `idCliente` INT NOT NULL AUTO_INCREMENT,
     `nomC` VARCHAR(20) NOT NULL,
     `apeC` VARCHAR(20) NOT NULL,
@@ -88,10 +88,10 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Cliente` (
     `pass` VARCHAR(20) NOT NULL,
     `Medidas_idMedidas` INT NOT NULL,
     PRIMARY KEY (`idCliente`),
-    INDEX `fk_Cliente_Medidas1_idx` (`Medidas_idMedidas` ASC) VISIBLE,
+    INDEX `fk_Cliente_Medidas1_idx` (`Medidas_idMedidas` ASC),
     CONSTRAINT `fk_Cliente_Medidas1`
         FOREIGN KEY (`Medidas_idMedidas`)
-        REFERENCES `mydb`.`Medidas` (`idMedidas`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`Medidas` (`idMedidas`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -100,22 +100,22 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Carrito`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Carrito` (
+CREATE TABLE IF NOT EXISTS `heroku_9bb4630feaaa67c`.`Carrito` (
     `id_sesion` VARCHAR(255) NOT NULL,
     `cantidad` DECIMAL(2) NOT NULL,
     `Cliente_idCliente` INT NOT NULL,
     `Producto_idProducto` INT NOT NULL,
     PRIMARY KEY (`id_sesion`),
-    INDEX `fk_Carrito_Cliente_idx` (`Cliente_idCliente` ASC) VISIBLE,
-    INDEX `fk_Carrito_Producto1_idx` (`Producto_idProducto` ASC) VISIBLE,
+    INDEX `fk_Carrito_Cliente_idx` (`Cliente_idCliente` ASC),
+    INDEX `fk_Carrito_Producto1_idx` (`Producto_idProducto` ASC),
     CONSTRAINT `fk_Carrito_Cliente`
         FOREIGN KEY (`Cliente_idCliente`)
-        REFERENCES `mydb`.`Cliente` (`idCliente`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`Cliente` (`idCliente`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION,
     CONSTRAINT `fk_Carrito_Producto1`
         FOREIGN KEY (`Producto_idProducto`)
-        REFERENCES `mydb`.`Producto` (`idProducto`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`Producto` (`idProducto`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -124,17 +124,17 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`entrega`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`entrega` (
+CREATE TABLE IF NOT EXISTS `heroku_9bb4630feaaa67c`.`entrega` (
     `identrega` INT NOT NULL,
     `direccion` VARCHAR(20) NOT NULL,
     `numeroEncargado` VARCHAR(20) NOT NULL,
     `nomEncargado` VARCHAR(45) NOT NULL,
     `Cliente_idCliente` INT NOT NULL,
     PRIMARY KEY (`identrega`),
-    INDEX `fk_entrega_Cliente1_idx` (`Cliente_idCliente` ASC) VISIBLE,
+    INDEX `fk_entrega_Cliente1_idx` (`Cliente_idCliente` ASC),
     CONSTRAINT `fk_entrega_Cliente1`
         FOREIGN KEY (`Cliente_idCliente`)
-        REFERENCES `mydb`.`Cliente` (`idCliente`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`Cliente` (`idCliente`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -143,22 +143,22 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`orden`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`orden` (
+CREATE TABLE IF NOT EXISTS `heroku_9bb4630feaaa67c`.`orden` (
     `idorden` INT NOT NULL,
     `total` DECIMAL(10,2) NOT NULL,
     `Cliente_idCliente` INT NOT NULL,
     `entrega_identrega` INT NOT NULL,
     PRIMARY KEY (`idorden`),
-    INDEX `fk_orden_Cliente1_idx` (`Cliente_idCliente` ASC) VISIBLE,
-    INDEX `fk_orden_entrega1_idx` (`entrega_identrega` ASC) VISIBLE,
+    INDEX `fk_orden_Cliente1_idx` (`Cliente_idCliente` ASC),
+    INDEX `fk_orden_entrega1_idx` (`entrega_identrega` ASC),
     CONSTRAINT `fk_orden_Cliente1`
         FOREIGN KEY (`Cliente_idCliente`)
-        REFERENCES `mydb`.`Cliente` (`idCliente`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`Cliente` (`idCliente`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION,
     CONSTRAINT `fk_orden_entrega1`
         FOREIGN KEY (`entrega_identrega`)
-        REFERENCES `mydb`.`entrega` (`identrega`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`entrega` (`identrega`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -167,7 +167,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`detalleOrden`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`detalleOrden` (
+CREATE TABLE IF NOT EXISTS `heroku_9bb4630feaaa67c`.`detalleOrden` (
     `iddetalleOrden` INT NOT NULL,
     `cantOrd` INT NOT NULL,
     `precio` DECIMAL(10,2) NOT NULL,
@@ -176,16 +176,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`detalleOrden` (
     `orden_idorden` INT NOT NULL,
     `Producto_idProducto` INT NOT NULL,
     PRIMARY KEY (`iddetalleOrden`),
-    INDEX `fk_detalleOrden_orden1_idx` (`orden_idorden` ASC) VISIBLE,
-    INDEX `fk_detalleOrden_Producto1_idx` (`Producto_idProducto` ASC) VISIBLE,
+    INDEX `fk_detalleOrden_orden1_idx` (`orden_idorden` ASC),
+    INDEX `fk_detalleOrden_Producto1_idx` (`Producto_idProducto` ASC),
     CONSTRAINT `fk_detalleOrden_orden1`
         FOREIGN KEY (`orden_idorden`)
-        REFERENCES `mydb`.`orden` (`idorden`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`orden` (`idorden`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION,
     CONSTRAINT `fk_detalleOrden_Producto1`
         FOREIGN KEY (`Producto_idProducto`)
-        REFERENCES `mydb`.`Producto` (`idProducto`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`Producto` (`idProducto`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -194,7 +194,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`metodoPago`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`metodoPago` (
+CREATE TABLE IF NOT EXISTS `heroku_9bb4630feaaa67c`.`metodoPago` (
     `idmetodoPago` INT NOT NULL AUTO_INCREMENT,
     `claveTransaccion` VARCHAR(250) NOT NULL,
     `paypalDatos` LONGTEXT NOT NULL,
@@ -202,10 +202,10 @@ CREATE TABLE IF NOT EXISTS `mydb`.`metodoPago` (
     `status` VARCHAR(20) NOT NULL,
     `detalleOrden_iddetalleOrden` INT NOT NULL,
     PRIMARY KEY (`idmetodoPago`),
-    INDEX `fk_metodoPago_detalleOrden1_idx` (`detalleOrden_iddetalleOrden` ASC) VISIBLE,
+    INDEX `fk_metodoPago_detalleOrden1_idx` (`detalleOrden_iddetalleOrden` ASC),
     CONSTRAINT `fk_metodoPago_detalleOrden1`
         FOREIGN KEY (`detalleOrden_iddetalleOrden`)
-        REFERENCES `mydb`.`detalleOrden` (`iddetalleOrden`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`detalleOrden` (`iddetalleOrden`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -214,23 +214,23 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`valoraciones`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`valoraciones` (
+CREATE TABLE IF NOT EXISTS `heroku_9bb4630feaaa67c`.`valoraciones` (
     `idvaloraciones` INT NOT NULL,
     `comentarios` VARCHAR(255) NOT NULL,
     `estrellas` INT NOT NULL,
     `Cliente_idCliente` INT NOT NULL,
     `Producto_idProducto` INT NOT NULL,
     PRIMARY KEY (`idvaloraciones`),
-    INDEX `fk_valoraciones_Cliente1_idx` (`Cliente_idCliente` ASC) VISIBLE,
-    INDEX `fk_valoraciones_Producto1_idx` (`Producto_idProducto` ASC) VISIBLE,
+    INDEX `fk_valoraciones_Cliente1_idx` (`Cliente_idCliente` ASC),
+    INDEX `fk_valoraciones_Producto1_idx` (`Producto_idProducto` ASC),
     CONSTRAINT `fk_valoraciones_Cliente1`
         FOREIGN KEY (`Cliente_idCliente`)
-        REFERENCES `mydb`.`Cliente` (`idCliente`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`Cliente` (`idCliente`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION,
     CONSTRAINT `fk_valoraciones_Producto1`
         FOREIGN KEY (`Producto_idProducto`)
-        REFERENCES `mydb`.`Producto` (`idProducto`)
+        REFERENCES `heroku_9bb4630feaaa67c`.`Producto` (`idProducto`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION)
 ENGINE = InnoDB;
