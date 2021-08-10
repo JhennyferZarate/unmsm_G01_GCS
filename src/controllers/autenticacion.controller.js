8ee9e0139e66ecf9ce33a2a8cb6c9cc47ee25533
arnold <luizarnolch@gmai.com>
2021-08-09 21:20:23 -0500

    Recuperado de correo: [01]error de redirección

--- src/controllers/autenticacion.controller.js
+++ src/controllers/autenticacion.controller.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const pool = require('../database');
-const nodemailer = require('nodemailer');
+const {transporter} = require('../lib/mailer');
 const { getMaxListeners } = require('../database');
 
 /**
@@ -24,44 +24,45 @@ const Iget = async(req, res) => {
  */
 
 const Rpget = async(req,res) =>{
-    res.render('auten/recuperar');
+    res.status(400).json({message: 'Get recuperar password'});
 }
 
 const Rppost = async(req,res) =>{
     const {mailC} = req.body;
 
-    const usuario = await pool.query('SELECT * FROM cliente where mailC = ?',[mailC]);
-
-    if(usuario>0){
+    const usuario = await pool.query('SELECT * FROM user_cliente WHERE mailC = ?',[mailC]);
+    const user = usuario[0];
+    if(usuario.length > 0){
         /**
          * Enviar contraseña a su correo
          */
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'g1.gcs.unmsm@gmail.com',
-                pass: 'G1.GCS.UNMSM'
-            }
-        });
-
-        transporter.verify().then(() =>{
-            console.log('Ready for send emails');
-        });
-
-        await transporter.sendMail({
-            from: "'Recuperación de contraseña' <g1.gcs.unmsm@gmail.com>",
-            to: mailC,
-            subject: 'Recuperación de contraseña',
-            html: `Su contraseña es: ${usuario.pass}`
-        });
+        try{
+            await transporter.sendMail({
+                from: "'Recuperación de contraseña' <g1.gcs.unmsm@gmail.com>",
+                //to: user.mailC,
+                to: user.mailC, 
+                subject: 'Recuperación de contraseña',
+                html: `Hola, tu contraseña es: `
+                // falta método de recuperar contraseña
+            });
+        } catch (error) {
+            emailStatus = error;
+            return res.status(400).json({ 
+                message:'Algo salio mal',
+                error: emailStatus
+            });
+        }
+        res.status(400).json({message: 'Se envio el correo'});
     } else {
+        res.status(400).json({
+            message: 'No se envio el correo',
+            mailC: `${mailC}`
+        });
         /**
          * req.flash('El usuario no ha sido registrado');
          */
     }
-    res.redirect('/ingreso');
+    //res.redirect('/ingreso');
 }
 
 /**
