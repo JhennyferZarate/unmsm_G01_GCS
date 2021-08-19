c522a92eac63764e263fe02854534c89cda17f4a
arnold <luizarnolch@gmai.com>
2021-08-19 16:25:33 -0500

    development

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -17,24 +17,9 @@ const error = async(req, res) => {
     res.render('index/error');
 }
 
-<<<<<<< HEAD
 const imagenes = async (req, res) => {
     res.render('index/imagenes');
 }
 
 
 module.exports = { index, nosotros, info, error, imagenes };
-=======
-const aux1 = async(req, res) => {
-    res.render('index/aux1');
-}
-
-const aux2 = async(req, res) => {
-    res.render('index/aux2');
-}
-const aux3 = async(req, res) => {
-    res.render('index/aux3');
-}
-
-module.exports = { index, nosotros, info, error, aux1, aux2, aux3 };
->>>>>>> 5225a616e27a02898660fc9e0cde3cf9eb0b502d
