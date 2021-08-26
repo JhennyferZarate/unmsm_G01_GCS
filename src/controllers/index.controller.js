3f80f2f30c2e57d879155887a80311a62685eee2
arnold <luizarnolch@gmai.com>
2021-08-25 19:37:12 -0500

    almacenamiento y petición  de imagenes

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const pool = require('../database');
+const fs = require('fs');
+const path = require('path');
+const { deepStrictEqual } = require('assert');
+const { formatWithOptions } = require('util');
 
 const index = async(req, res) => {
-    res.render('index/index');
+    const modelos = await pool.query("SELECT * FROM modelos");
+
+    console.log(modelos);
+    
+    for(i=0;i<modelos.length;i++){
+        fs.writeFileSync(path.join(__dirname,'../public/images/dbimagenes/' + modelos[i].id + '-' + modelos[i].modelo + '.png'),modelos[i].ruta_imagen)
+    }
+    
+    /*
+    modelos.map(
+        fs.writeFileSync(path.join(__dirname,'../public/images/dbimagenes/' + modelos.id + '-' + modelos.modelo + '.png'),modelos.ruta_imagen)
+    );
+    */
+    
+
+    const imagenes = fs.readdirSync(path.join(__dirname, '../public/images/prendas/'));
+
+    const Imagenes = new Array();
+
+    for(i=0;i<imagenes.length;i++){
+        var aux = {
+            "name": imagenes[i]
+        }
+        Imagenes.push(aux);
+    }
+    //console.log(Imagenes);
+    res.render('index/index',{Imagenes});
 }
 
 const nosotros = async(req, res) => {
+    
+
     res.render('index/nosotros');
 }
 
