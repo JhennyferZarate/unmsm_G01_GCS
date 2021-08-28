b86fd87f42a06e71fbef3fe4e551518184800647
arnold <luizarnolch@gmai.com>
2021-08-28 13:20:52 -0500

    GCS3_db

--- src/keys.js
+++ src/keys.js
@@ -10,3 +10,8 @@ module.exports = {
 };
 
 //mysql://b9911ef0d9abb6:2bbcf3fe@us-cdbr-east-04.cleardb.com/heroku_9bb4630feaaa67c?reconnect=true
+
+
+/**GCS3
+ * mysql://bacd3089767c91:778dacd5@us-cdbr-east-04.cleardb.com/heroku_7acd253dba63c7f?reconnect=true
+ */
